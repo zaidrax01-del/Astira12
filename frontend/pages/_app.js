55a6656eb7aa9cf34fd275139dfd44542485698a
danofabupaq764-beep <danofabupaq764@gmail.com>
2026-05-23 17:37:18 +0100

    Update _app.js

--- frontend/pages/_app.js
+++ frontend/pages/_app.js
@@ -1,1 +1,28 @@
+import '../styles/globals.css'
+import { useMemo } from 'react'
+import { ConnectionProvider, WalletProvider } from '@solana/wallet-adapter-react'
+import { WalletModalProvider } from '@solana/wallet-adapter-react-ui'
+import { PhantomWalletAdapter, SolflareWalletAdapter } from '@solana/wallet-adapter-wallets'
+import { clusterApiUrl } from '@solana/web3.js'
+import { Web3ContextProvider } from '../context/Web3Context'
+import '@solana/wallet-adapter-react-ui/styles.css'
 
+function MyApp({ Component, pageProps }) {
+  const network = 'mainnet-beta'
+  const endpoint = useMemo(() => clusterApiUrl(network), [network])
+  const wallets = useMemo(() => [new PhantomWalletAdapter(), new SolflareWalletAdapter()], [])
+
+  return (
+    <ConnectionProvider endpoint={endpoint}>
+      <WalletProvider wallets={wallets} autoConnect>
+        <WalletModalProvider>
+          <Web3ContextProvider>
+            <Component {...pageProps} />
+          </Web3ContextProvider>
+        </WalletModalProvider>
+      </WalletProvider>
+    </ConnectionProvider>
+  )
+}
+
+export default MyApp
