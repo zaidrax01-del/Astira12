616f033f06c08f24f2ea014b2c7b0bdd53aa1cea
danofabupaq764-beep <danofabupaq764@gmail.com>
2026-05-23 18:29:00 +0100

    Update _app.js

--- frontend/pages/_app.js
+++ frontend/pages/_app.js
@@ -2,7 +2,9 @@ import '../styles/globals.css'
 import { useMemo } from 'react'
 import { ConnectionProvider, WalletProvider } from '@solana/wallet-adapter-react'
 import { WalletModalProvider } from '@solana/wallet-adapter-react-ui'
-import { PhantomWalletAdapter, SolflareWalletAdapter } from '@solana/wallet-adapter-wallets'
+import { PhantomWalletAdapter } from '@solana/wallet-adapter-phantom'
+import { SolflareWalletAdapter } from '@solana/wallet-adapter-solflare'
+import { TrustWalletAdapter } from '@solana/wallet-adapter-trust'
 import { clusterApiUrl } from '@solana/web3.js'
 import { Web3ContextProvider } from '../context/Web3Context'
 import '@solana/wallet-adapter-react-ui/styles.css'
@@ -10,7 +12,14 @@ import '@solana/wallet-adapter-react-ui/styles.css'
 function MyApp({ Component, pageProps }) {
   const network = 'mainnet-beta'
   const endpoint = useMemo(() => clusterApiUrl(network), [network])
-  const wallets = useMemo(() => [new PhantomWalletAdapter(), new SolflareWalletAdapter()], [])
+  const wallets = useMemo(
+    () => [
+      new PhantomWalletAdapter(),
+      new SolflareWalletAdapter(),
+      new TrustWalletAdapter(),
+    ],
+    []
+  )
 
   return (
     <ConnectionProvider endpoint={endpoint}>
