edf92f6110fd4f193e3282b5705cbed43075657a
danofabupaq764-beep <danofabupaq764@gmail.com>
2026-05-23 17:38:18 +0100

    Update index.js

--- frontend/pages/index.js
+++ frontend/pages/index.js
@@ -1,1 +1,16 @@
+'use client'
+import Navbar from '../components/layout/Navbar'
+import Hero from '../components/sections/Hero'
+import Featured from '../components/sections/Featured'
+import SpaceBackground from '../components/animations/SpaceBackground'
 
+export default function Home() {
+  return (
+    <div className="relative min-h-screen overflow-hidden">
+      <SpaceBackground />
+      <Navbar />
+      <Hero />
+      <Featured />
+    </div>
+  )
+}
