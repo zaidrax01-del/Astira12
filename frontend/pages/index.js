1b45878b4b5411f258e4a966d5fc5b6980659508
danofabupaq764-beep <danofabupaq764@gmail.com>
2026-05-23 21:44:05 +0100

    Update index.js

--- frontend/pages/index.js
+++ frontend/pages/index.js
@@ -7,9 +7,16 @@ import SpaceBackground from '../components/animations/SpaceBackground'
 export default function Home() {
   return (
     <div className="relative min-h-screen overflow-hidden">
+      {/* Deep space stars behind everything */}
       <SpaceBackground />
+
+      {/* Transparent navbar */}
       <Navbar />
+
+      {/* Hero section with new background image */}
       <Hero />
+
+      {/* Featured planets grid */}
       <Featured />
     </div>
   )
