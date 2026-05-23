43a9603cecc54d321b61e86a430432a42b4e3008
danofabupaq764-beep <danofabupaq764@gmail.com>
2026-05-23 17:53:18 +0100

    Update marketplace.js

--- frontend/pages/marketplace.js
+++ frontend/pages/marketplace.js
@@ -1,1 +1,16 @@
+'use client'
+import Navbar from '../components/layout/Navbar'
+import SpaceBackground from '../components/animations/SpaceBackground'
 
+export default function Marketplace() {
+  return (
+    <div className="relative min-h-screen">
+      <SpaceBackground />
+      <Navbar />
+      <main className="pt-24 px-8 text-center">
+        <h2 className="text-4xl font-bold text-gradient">Galactic Marketplace</h2>
+        <p className="text-gray-300 mt-4">Coming soon.</p>
+      </main>
+    </div>
+  )
+}
